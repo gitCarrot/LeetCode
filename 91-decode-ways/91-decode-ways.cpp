b339519f8eccbedf98fc478e6a65ce326bbab38c
carrotHong <96459232+carrotHong@users.noreply.github.com>
2022-10-01 22:42:16 +0900

    Time: 4 ms (47.86%), Space: 6.2 MB (49.09%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if(n==0 || s[0]=='0') return 0;
+        vector<int> f(n+1,1);
+        for(int i=2;i<=n;i++)
+        {
+            if(s[i-1]=='0') f[i]=0;
+            else f[i] = f[i-1];
+            if(s[i-2]=='1' || s[i-2]=='2' && s[i-1]<='6')
+                f[i] += f[i-2];
+        }
+        return f[n];
+    }
+};
