1c6a5c9c142f7a9d9a6836799b7087f5924a735f
carrotHong <96459232+carrotHong@users.noreply.github.com>
2022-10-24 15:31:14 +0900

    Time: 10 ms (86.91%), Space: 8 MB (99.91%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int maxlength = 0;
-    void recur(vector<string>& arr, int pos, int length, int BIT){
-        if(pos == arr.size()) {
-            maxlength = max(maxlength, length); return;
-        }
-        recur(arr, pos +1, length, BIT);
-        for(char c : arr[pos]) {
-            if(1 << (c - 'a') & BIT) return;
-            BIT |= 1 << (c - 'a');
-        }
-        recur(arr, pos +1, length + arr[pos].length() , BIT);
-    }
+    
     int maxLength(vector<string>& arr) {
-        recur(arr, 0,0,0);
+        function<void(int,int,int)> recur = [&](int p, int l, int BIT) -> void {
+            if(p == arr.size()) {
+                maxlength = max(maxlength, l); return;
+            }
+            recur(p +1, l, BIT);
+            for(char c : arr[p]) {
+                if(1 << (c - 'a') & BIT) return;
+                BIT |= 1 << (c - 'a');
+            }
+            recur(p +1, l + arr[p].length() , BIT);
+        };
+        recur(0,0,0);
         return maxlength;
     }
 };
