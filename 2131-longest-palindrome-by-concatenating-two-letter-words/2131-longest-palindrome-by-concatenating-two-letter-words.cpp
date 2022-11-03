29a68544c969a331a2cbba97a1234089f6f3ae5b
carrotHong <96459232+carrotHong@users.noreply.github.com>
2022-11-03 12:44:18 +0900

    Time: 467 ms (85.62%), Space: 168.3 MB (20.85%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,31 +1,22 @@
-bool isPalin(string str){
-    int l = 0, r = str.length()-1;
-    while(l <= r){
-        if(str[l] != str[r]) return false;
-        l++; r--;
-    }
-    return true;
-}
+
 
 class Solution {
     
 public:
     int longestPalindrome(vector<string>& words) {
-        unordered_map<string, int> mp;
-        int total = 0, onePelin = 0, pel = 0;
-        for(string word : words){
-            bool isPal = isPalin(word); mp[word]++;
-            if(isPal){
-                if(mp[word] % 2 == 1) onePelin++;
-                else --onePelin;
-            }
-            string temp = word;
-            reverse(temp.begin(), temp.end());
-            if(!isPal && mp[temp] >= mp[word]) total += 4;
-            if(isPal && mp[temp] % 2 == 0) total += 4;
-        }
-        
-        return total + (onePelin > 0 ? 2 : 0);
-        
+    vector<vector<int>> counter(26, vector<int>(26, 0));
+    int ans = 0;
+    for (string w: words) {
+        int a = w[0] - 'a', b = w[1] - 'a';
+        if (counter[b][a]) ans += 4, counter[b][a]--;
+        else counter[a][b]++;
     }
+    for (int i = 0; i < 26; i++) {
+        if (counter[i][i]) {
+            ans += 2;
+            break;
+        }
+    }
+    return ans;
+}
 };
