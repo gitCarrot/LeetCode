1092d7664b0cffe9e63c1ebc746c7d507d342691
gitCarrot <96459232+gitCarrot@users.noreply.github.com>
2023-04-22 13:59:04 -1000

    Time: 815 ms (80.23%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,3 +1,1 @@
-# Please write a DELETE statement and DO NOT write a SELECT statement.
-# Write your MySQL query statement below
-delete p1 from person p1, person p2 where p1.id > p2.id and p1.email = p2.email;
+delete from Person where id not in (select min(id) from person group by email);
