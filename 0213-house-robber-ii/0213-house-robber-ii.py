03e7f4adcae7e31c1db6dbfb96ca407ee6a1f5a7
gitCarrot <96459232+gitCarrot@users.noreply.github.com>
2023-05-02 14:37:30 -1000

    Time: 53 ms (5.16%), Space: 16.5 MB (5.88%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,23 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        n = len(nums)
+        @lru_cache(None)
+        def solvelast(n):
+            if n >= len(nums): return 0
+            return max(nums[n] + solvelast(n+2), solvelast(n+1))
         
-		# function when considering the first and neglecting last house
-        @functools.lru_cache(None)
-        def util1(i):
-            if i >= n-1:
-                return 0
-			
-            return max(nums[i] + util1(i+2), util1(i+1))
+        @lru_cache(None)
+        def solvefirst(n):
+            if n >= len(nums)-1: return 0
+            return max(nums[n] + solvefirst(n+2), solvefirst(n+1))
         
-		# function when considering the last and neglecting first house
-        @functools.lru_cache(None)
-        def util2(i):
-            if i >= n:
-                return 0
-            
-            return max(nums[i] + util2(i+2), util2(i+1))
-        
-        return max(util1(0), util2(1), nums[0])
+        return max(solvefirst(0), solvelast(1), nums[0])
         
