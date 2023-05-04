256fea7d632cae1751a16b62930b2eed68e20079
gitCarrot <96459232+gitCarrot@users.noreply.github.com>
2023-05-04 00:39:16 -1000

    Time: 7850 ms (5.00%), Space: 57.1 MB (5.08%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        @lru_cache(None)
+        def canJump(idx):
+            if idx == len(nums)-1: return True
+            pos = False
+            if nums[idx] != 0:
+                for i in range(idx+1, min(idx+nums[idx]+1, len(nums))):
+                    if canJump(i) : return True
+            return False
+        
+        return canJump(0)
+            
+        
