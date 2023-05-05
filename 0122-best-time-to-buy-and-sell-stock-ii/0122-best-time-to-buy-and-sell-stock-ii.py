f2426c393df0faa4a1af2a618b3acb78d5b15d47
gitCarrot <96459232+gitCarrot@users.noreply.github.com>
2023-05-05 00:59:44 -1000

    Time: 126 ms (5.04%), Space: 56 MB (5.27%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -5,9 +5,7 @@ class Solution:
         @lru_cache(None)
         def maxProfit(idx, canBuy):
             if idx == len(prices): return 0
-            if canBuy:
-                return max(maxProfit(idx+1, 0) - prices[idx], maxProfit(idx+1, 1))
-            else: return max(maxProfit(idx+1, 1) + prices[idx], maxProfit(idx+1, 0))
+            return max(maxProfit(idx+1, 0) - prices[idx], maxProfit(idx+1, 1)) if canBuy else max(maxProfit(idx+1, 1) + prices[idx], maxProfit(idx+1, 0))
             
         return maxProfit(0, 1)
                 
