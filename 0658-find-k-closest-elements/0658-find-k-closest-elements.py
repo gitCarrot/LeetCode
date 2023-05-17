5ff9435eee4b0b2474bfaf4e9d24f71698130ec5
gitCarrot <96459232+gitCarrot@users.noreply.github.com>
2023-05-16 15:49:17 -1000

    Time: 280 ms (91.62%), Space: 18 MB (12.86%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,51 +1,24 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         
-        def lb(arr, x):
-            l, r = 0, len(arr)-1
+        def lb(arr, x, k):
+            l, r = 0, len(arr)-k-1
             ans = len(arr)
             while l<=r:
                 mid = (l+r)//2
-                if arr[mid] >= x:
-                    ans = mid
-                    r = mid -1
-                else:
+                if x - arr[mid] > arr[mid+k] - x:
                     l = mid + 1
-                    
-            return ans
+                else:
+                    r = mid -1
+            return l
                  
-                
-        idx = lb(arr, x)
-        if idx == len(arr): idx -= 1
-        elif idx >= 1 and arr[idx] - x >= x - arr[idx-1]:
-            idx -= 1
-
-        anslist = []
-        
-        k -= 1
-        anslist.append(arr[idx])
-        
-        lidx, ridx = idx -1, idx + 1
-        while k:
-            k-=1
-            if lidx < 0: 
-                anslist.append(arr[ridx])
-                ridx += 1
-            elif ridx >= len(arr):
-                anslist.append(arr[lidx])
-                lidx -= 1
-            else:
-                if x - arr[lidx] <= arr[ridx] - x:
-                    anslist.append(arr[lidx])
-                    lidx -= 1
-                elif x - arr[lidx] > arr[ridx] - x:
-                    anslist.append(arr[ridx])
-                    ridx += 1
                     
         
-            
+        idx = lb(arr, x, k)
+        
+        print(idx)
                     
-        return sorted(anslist)
+        return arr[idx:idx+k]
         
         
         
