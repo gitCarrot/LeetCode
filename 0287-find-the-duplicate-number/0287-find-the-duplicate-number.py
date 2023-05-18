6e61b2499056db19082b414351bce702869852f9
gitCarrot <96459232+gitCarrot@users.noreply.github.com>
2023-05-17 17:39:21 -1000

    Time: 594 ms (92.15%), Space: 34.5 MB (8.75%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        
+        dic = defaultdict(int)
+        
+        for num in nums:
+            if dic[num] == 1:
+                return num
+            else:
+                dic[num] = 1
+                
+        
+        return 0
+        
