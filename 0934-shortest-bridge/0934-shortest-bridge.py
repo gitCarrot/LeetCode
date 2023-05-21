0a5cc7f0a840ae033402bc8a127cc4c1592b32d4
gitCarrot <96459232+gitCarrot@users.noreply.github.com>
2023-05-20 16:54:23 -1000

    Time: 364 ms (92.50%), Space: 17.1 MB (79.03%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -9,37 +9,36 @@ class Solution:
             nonlocal ans
             q = deque()
             q2 = deque()
-            q.append((i,j,0,0))
-            q2.append((i,j,0,0))
+            q.append((i,j,0))
+            q2.append((i,j,0))
             visited[i][j] = 1           
             while q:
-                cr, cc, dist, cs = q.popleft()
+                cr, cc, dist = q.popleft()
                 for dr, dc in ((-1,0), (1, 0), (0, 1), (0, -1)):
                     nr, nc = cr + dr, cc + dc
                     if nr < 0 or nc < 0 or nr >= n or nc >= n: continue
                     if visited[nr][nc]: continue
                     if grid[nr][nc] == 1:
                         visited[nr][nc] = 1
-                        q.append((nr,nc,0,0))
-                        q2.append((nr,nc,0,0))
+                        q.append((nr,nc,0))
+                        q2.append((nr,nc,0))
                         
             while q2:
-                cr, cc, dist, cs = q2.popleft()
-                if grid[cr][cc] == 1 and cs == 1:
-                    ans = min(ans, dist-1)
+                cr, cc, dist = q2.popleft()
                 for dr, dc in ((-1,0), (1, 0), (0, 1), (0, -1)):
                     nr, nc = cr + dr, cc + dc
                     if nr < 0 or nc < 0 or nr >= n or nc >= n: continue
                     if visited[nr][nc]: continue
+                    if grid[nr][nc] == 1: return dist
                     visited[nr][nc] = 1
-                    q2.append((nr,nc,dist+1, 1))      
+                    q2.append((nr,nc,dist+1))      
                         
         
         for i in range(n):
             for j in range(n):
                 if grid[i][j] == 1:
-                    bfs(i,j)
-                    return ans
+                    return bfs(i,j)
+                    
                     
         
         
